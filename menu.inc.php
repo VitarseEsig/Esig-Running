8cb9dbc0e24b3249bd5085255ce0dd66a53e77b8
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-10-18 01:18:44 +0200

    Ajout du design du titre du logo

--- menu.inc.php
+++ menu.inc.php
@@ -1,10 +1,10 @@
 <header>
     <nav class="navbar navbar-expand-lg light">
-        <div class="container-fluid">
+        <div class="container-fluid" id="divNav1">
             <a class="navbar-brand" id="logo" href="#">
-                <img src="./assets/img/logo-esigelec.png" alt="logo esigelec" class="d-inline-block align text-top">
+                <img src="./assets/img/logo-esigelec.png" alt="logo esigelec" class="d-inline-block align text-top" class="img-fluid">
                 <h1 class="titre-logo">Esig'Running</h1>
-                <img src="./assets/img/bonhomme running.png" alt="homme qui court">
+                <img src="./assets/img/bonhomme running.png" alt="homme qui court" class="img-fluid" id="logo-man">
             </a>
         </div>
         
