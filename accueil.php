7f105c04d2d0584c8bd526e2851936606aa3fabf
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-10-25 12:03:40 +0200

    Ajout partiel du background de l'accueil

--- accueil.php
+++ accueil.php
@@ -9,11 +9,22 @@
     <h2 class="ombre-bleue margin-debut">TRANSPIRE<br>L'INTELLIGENCE</h2>
 </div>
 
-<section class="d-flex align-items-center" id="section-debut">
-    <img src="./assets/img/runner avec mur.jpg" alt="homme qui court" class="img-fluid taille-img">
-    <div class="d-flex flex-column justify-content-center align-items-center cellule-texte">
-        <p>Courir n'a jamais été aussi amusant ! Esig'Running est le club d'athlétisme de l'ESIGELEC pour ses étudiants et personnel. Parce qu'on ne peut travailler avec des personnes malades, vous devez prendre soin de votre corps ! Rejoignez la course !</p>
+<section class="row align-items-center g-0 g-sm-0" id="section-debut">
+    <div class="col-12 col-sm-5">
+        <img src="./assets/img/runner avec mur.jpg" alt="homme qui court" class="img-fluid taille-img">
     </div>
+    
+    
+    <div class="col-12 col-sm-7">
+        <div class="superposition-parent">
+            <div class="d-flex flex-column justify-content-center align-items-center cellule-texte superposition-enfant">
+                <p>Courir n'a jamais été aussi amusant ! Esig'Running est le club d'athlétisme de l'ESIGELEC pour ses étudiants et personnel. Parce qu'on ne peut travailler avec des personnes malades, vous devez prendre soin de votre corps ! Rejoignez la course !</p>
+            </div>
+            <img src="./assets/img/Design special.png" alt="design special" class="superposition-enfant-bg img-fluid">
+        </div>
+    </div>
+    
+    
 </section>
 
 <?php
