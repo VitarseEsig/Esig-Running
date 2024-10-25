df4772bcbf3490941122f7e51ceea0a24fa558c1
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-10-25 10:36:42 +0200

    Ajout des éléments de la page d'accueil

--- accueil.php
+++ accueil.php
@@ -6,10 +6,15 @@
 ?>
 
 <div class="d-flex justify-content-center">
-    <h2>Transpire<br>l'intelligence</h2>
+    <h2 class="ombre-bleue margin-debut">TRANSPIRE<br>L'INTELLIGENCE</h2>
 </div>
 
-<p>Vous êtes sur la page d'accueil</p>
+<section class="d-flex align-items-center" id="section-debut">
+    <img src="./assets/img/runner avec mur.jpg" alt="homme qui court" class="img-fluid taille-img">
+    <div class="d-flex flex-column justify-content-center align-items-center cellule-texte">
+        <p>Courir n'a jamais été aussi amusant ! Esig'Running est le club d'athlétisme de l'ESIGELEC pour ses étudiants et personnel. Parce qu'on ne peut travailler avec des personnes malades, vous devez prendre soin de votre corps ! Rejoignez la course !</p>
+    </div>
+</section>
 
 <?php
 
