4e496c14636de6bdc8f58da84d13dd83810515fe
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-10-26 20:49:19 +0200

    Ajout de nouveaux éléments sur la page d'accueil

--- accueil.php
+++ accueil.php
@@ -10,12 +10,12 @@
 </div>
 
 <section class="row align-items-center g-0 g-sm-0" id="section-debut">
-    <div class="col-12 col-sm-5">
+    <div class="col-12 col-sm-5 offset-3 offset-sm-0">
         <img src="./assets/img/runner avec mur.jpg" alt="homme qui court" class="img-fluid taille-img">
     </div>
     
     
-    <div class="col-12 col-sm-7">
+    <div class="col-12 col-sm-7 offset-3 offset-sm-0">
         <div class="superposition-parent">
             <div class="d-flex flex-column justify-content-center align-items-center cellule-texte superposition-enfant">
                 <p>Courir n'a jamais été aussi amusant ! Esig'Running est le club d'athlétisme de l'ESIGELEC pour ses étudiants et personnel. Parce qu'on ne peut travailler avec des personnes malades, vous devez prendre soin de votre corps ! Rejoignez la course !</p>
@@ -24,8 +24,17 @@
             <img src="./assets/img/chaussure.png" alt="chaussure peu opaque" class="img-fluid superposition-enfant-bg-2">
         </div>
     </div>
-    
-    
+</section>
+
+<div class="margin-haut row">
+    <img src="./assets/img/Traces de pas.png" alt="traces de pas" class="col-3 offset-3">
+</div>
+
+<section id="section-milieu" class="superposition-parent">
+    <div class="d-flex flex-column justify-content-center align-items-start cellule-texte cellule-2">
+        <h2 class="ombre-bleue margin-h2">PRENDS SOIN<br>DE TON CORPS</h2>
+        <p>On ne peut bien évidemment pas pratiquer une activité sportive comme un bourrin ! Esig'Running met l'accent sur l'hydratation ainsi que les étirements. Chez nous tu apprendras de nouvelles techniques d'étirements !</p>
+    </div>
 </section>
 
 <?php
