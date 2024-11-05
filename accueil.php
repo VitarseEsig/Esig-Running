0ef3df5b43e6f23379e270b6cc0197e511ff663d
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-11-05 11:03:31 +0100

    Ajout d'autres éléments de la page d'accueil

--- accueil.php
+++ accueil.php
@@ -20,8 +20,8 @@
             <div class="d-flex flex-column justify-content-center align-items-center cellule-texte superposition-enfant">
                 <p>Courir n'a jamais été aussi amusant ! Esig'Running est le club d'athlétisme de l'ESIGELEC pour ses étudiants et personnel. Parce qu'on ne peut travailler avec des personnes malades, vous devez prendre soin de votre corps ! Rejoignez la course !</p>
             </div>
-            <img src="./assets/img/Design special.png" alt="design special" class="superposition-enfant-bg img-fluid">
-            <img src="./assets/img/chaussure.png" alt="chaussure peu opaque" class="img-fluid superposition-enfant-bg-2">
+            <img src="./assets/img/Design special.png" alt="design special" class="superposition-enfant-bg img-fluid" id="design-special-debut">
+            <img src="./assets/img/chaussure.png" alt="chaussure peu opaque" class="img-fluid superposition-enfant-bg" id="chaussure-debut">
         </div>
     </div>
 </section>
@@ -30,10 +30,35 @@
     <img src="./assets/img/Traces de pas.png" alt="traces de pas" class="col-3 offset-3">
 </div>
 
-<section id="section-milieu" class="superposition-parent">
-    <div class="d-flex flex-column justify-content-center align-items-start cellule-texte cellule-2">
-        <h2 class="ombre-bleue margin-h2">PRENDS SOIN<br>DE TON CORPS</h2>
-        <p>On ne peut bien évidemment pas pratiquer une activité sportive comme un bourrin ! Esig'Running met l'accent sur l'hydratation ainsi que les étirements. Chez nous tu apprendras de nouvelles techniques d'étirements !</p>
+<section id="section-milieu" class="row align-items-center g-0 g-sm-0">
+    <div class="col-8 superposition-parent">
+        <div class="d-flex flex-column justify-content-center align-items-start cellule-texte cellule-2 superposition-enfant">
+            <h2 class="ombre-bleue margin-h2">PRENDS SOIN<br>DE TON CORPS</h2>
+            <p>On ne peut bien évidemment pas pratiquer une activité sportive comme un bourrin ! Esig'Running met l'accent sur l'hydratation ainsi que les étirements. Chez nous tu apprendras de nouvelles techniques d'étirements !</p>
+        </div>
+        <img src="./assets/img/Design special.png" alt="design special" class="img-fluid superposition-enfant-bg" id="design-special-milieu">
+        <img src="./assets/img/etoile.png" alt="etoile rose" class=" superposition-enfant-bg" id="etoile">
+        <img src="./assets/img/chaussure plus opaque.png" alt="chaussure plus opaque" class="img-fluid superposition-enfant-bg" id="chaussure-milieu-1">
+        <img src="./assets/img/Traces de pas.png" alt="traces de pas" class="img-fluid superposition-enfant-bg" id="traces-de-pas-milieu">
+        <img src="./assets/img/chaussure.png" alt="chaussure moins opaque" class="img-fluid superposition-enfant-bg" id="chaussure-milieu-2">
+        
+    </div>
+    <img src="./assets/img/femme buvant eau.png" alt="femme qui boit de l'eau" class="col-4 img-fluid">
+</section>
+
+<section id="section-entrainements" class="d-flex flex-column justify-content-center align-items-center">
+    <h2 class="ombre-bleue">DES ENTRAINEMENTS POUR TOUT LE MONDE</h2>
+    <div class="cellule-texte">
+
+    </div>
+    <div class="cellule-texte">
+
+    </div>
+    <div class="cellule-texte">
+
+    </div>
+    <div class="cellule-texte">
+
     </div>
 </section>
 
