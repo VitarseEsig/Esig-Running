90cfcac351422802fd71ef66fec12a97b9b6835e
VitarseEsig <arthur.amisi@groupe-esigelec.org>
2024-11-13 12:31:23 +0100

    Design de la page des comptes

--- compte.php
+++ compte.php
@@ -11,7 +11,20 @@
     } else {
         header("Location: ./connexion.php"); // Redirection si pas de session active
         }
-    echo "<h2 class="."ombre-bleue".">Bonjour ".$prenom." ".$nom."</h2>";
-
+    echo "<div class="."centre"." id="."bienvenue".">
+                <h2 class='ombre-bleue'>Bienvenue ".$prenom."</h2></div>";
+    echo "<section class='d-flex flex-column'>
+            <div class= 'row align-items-start' id='tab-bord'>
+                <h2 class='ombre-bleue offset-2'>Tableau de bord</h2>
+            </div>
+            <div class='row'>
+                <div class='col-8 d-flex justify-content-center align-items-center' id='entrainements-a-venir'>
+                    <h3 class='ombre-bleue'>Entraînements à venir</h3>
+                </div>
+                <div class='col-4 row ' id='btn-rouge'>
+                    <h3 class=' ombre-bleue'>Se désinscrire</h3>
+                </div>
+            </div>
+          </section>";
 ?>
 <!-- <h2 class="ombre-bleue">Bonjour .$utilisateur. !</h2> -->
